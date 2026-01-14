463c411d8347596f055edb4bd1acbd2db296cb6f
Eunseo-ee <2whwhwh@gmail.com>
2026-01-15 00:32:54 +0900

    feat: implement common search response DTO

--- demo/src/main/java/book_log/demo/dto/response/UnifiedSearchResponse.java
+++ demo/src/main/java/book_log/demo/dto/response/UnifiedSearchResponse.java
@@ -1,6 +1,18 @@
 package book_log.demo.dto.response;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import book_log.demo.domain.Category;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 
+@Getter @Builder
+@NoArgsConstructor @AllArgsConstructor
 public class UnifiedSearchResponse {
-    
+    private String title;
+    private String thumbnailUrl;
+    private String externalId;
+    private String authorOrDirector;
+    // 우리가 정해놓은 Category Enuim에 맞게 내보내도록
+    private Category category;
 }
