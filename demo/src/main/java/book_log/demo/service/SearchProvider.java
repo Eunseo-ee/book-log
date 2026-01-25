bda4e008c8cfada914d7ff84b59c00dfa336e94d
Eunseo-ee <2whwhwh@gmail.com>
2026-01-26 01:47:26 +0900

    feat: add SearchProvider

--- demo/src/main/java/book_log/demo/service/SearchProvider.java
+++ demo/src/main/java/book_log/demo/service/SearchProvider.java
@@ -1,5 +1,11 @@
 package book_log.demo.service;
 
-public class SearchProvider {
-    
+import java.util.List;
+
+import book_log.demo.domain.Category;
+import book_log.demo.dto.response.UnifiedSearchResponse;
+
+public interface SearchProvider {
+    List<UnifiedSearchResponse> search(String query);
+    boolean supports(Category category);
 }
