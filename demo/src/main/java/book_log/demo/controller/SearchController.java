6abb58a9e05eacf34358ae181257221c5e8c2dc3
Eunseo-ee <2whwhwh@gmail.com>
2026-03-11 12:39:57 +0900

    feature: SearchService, Controller

--- demo/src/main/java/book_log/demo/controller/SearchController.java
+++ demo/src/main/java/book_log/demo/controller/SearchController.java
@@ -1,5 +1,21 @@
 package book_log.demo.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import book_log.demo.dto.response.UnifiedSearchResponse;
+import book_log.demo.service.SearchService;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/api/search")
+@RequiredArgsConstructor
+
 public class SearchController {
-    
+    private final SearchService searchService;
+
+    @GetMapping
+    public ResponseEntity<List<UnifiedSearchResponse>> search()
 }
--- demo/src/main/java/book_log/demo/service/SearchService.java
+++ demo/src/main/java/book_log/demo/service/SearchService.java
@@ -1,5 +1,27 @@
 package book_log.demo.service;
 
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import book_log.demo.dto.response.UnifiedSearchResponse;
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+
 public class SearchService {
-    
+    private final KakaoSearchService kakaoSearchService;
+    private final TmdbSearchService tmdbSearchService;
+    private final AnimeSearchService animeSearchService;
+
+    public List<UnifiedSearchResponse> search(String category, String query) {
+        return switch (category.toUpperCase()) {
+            case "BOOK" -> kakaoSearchService.search(query);
+            case "MOVIE" -> tmdbSearchService.search(query);
+            case "DRAMA" -> animeSearchService.search(query);
+            case "ANIME" -> animeSearchService.search(query);
+            default -> throw new IllegalArgumentException("지원하지 않는 카테고리입니다.");
+        };
+    }
 }
