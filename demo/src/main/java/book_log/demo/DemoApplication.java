b2e765f883269d4ca9e1e3e73c68821fb4fe8505
Eunseo-ee <2whwhwh@gmail.com>
2026-03-14 20:30:47 +0900

    config: RestTemplate 빈 설정 추가

--- demo/src/main/java/book_log/demo/DemoApplication.java
+++ demo/src/main/java/book_log/demo/DemoApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class DemoApplication {
@@ -12,12 +13,17 @@ public class DemoApplication {
         SpringApplication.run(DemoApplication.class, args);
     }
 
-    // 서버가 시작될 때 실행되는 코드
+    // 통신 도구(전화기)를 스프링에 등록
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+    // 서버 시작 테스트용
     @Bean
     public CommandLineRunner demo() {
         return (args) -> {
-            System.out.println("데이터 저장 테스트 시작!");
-            // 여기에 나중에 Repository를 주입받아 save하는 코드를 넣을 수 있습니다.
+            System.out.println("서버가 정상적으로 시작되었습니다!");
         };
     }
 }
