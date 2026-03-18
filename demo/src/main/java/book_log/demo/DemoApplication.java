5c5b4fa915c9405d73d44268e8c05509883016f1
Eunseo-ee <2whwhwh@gmail.com>
2026-03-18 17:31:31 +0900

    refactor: RestTemplate 설정을 별도 Config 클래스로 분리

--- demo/src/main/java/book_log/demo/DemoApplication.java
+++ demo/src/main/java/book_log/demo/DemoApplication.java
@@ -4,19 +4,12 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class DemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
-    }
-
-    // 통신 도구(전화기)를 스프링에 등록
-    @Bean
-    public RestTemplate restTemplate() {
-        return new RestTemplate();
     }
 
     // 서버 시작 테스트용
--- demo/src/main/java/book_log/demo/config/RestTemplateConfig.java
+++ demo/src/main/java/book_log/demo/config/RestTemplateConfig.java
@@ -1,5 +1,14 @@
 package book_log.demo.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration // 이 클래스가 설정 파일임을 스프링에게 알려줌
 public class RestTemplateConfig {
     
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
 }
