78b9c70b4512a4358028af5babdb148fa427b00f
Eunseo-ee <2whwhwh@gmail.com>
2026-03-19 20:02:46 +0900

    feat: AniList API 연동

--- demo/src/main/java/book_log/demo/dto/response/AnimeResponse.java
+++ demo/src/main/java/book_log/demo/dto/response/AnimeResponse.java
@@ -1,5 +1,50 @@
 package book_log.demo.dto.response;
 
+import lombok.Getter;
+import lombok.Setter;
+import java.util.List;
+
+@Getter @Setter
 public class AnimeResponse {
-    
+    private Data data;
+
+    @Getter @Setter
+    public static class Data {
+        private Page Page;
+    }
+
+    @Getter @Setter
+    public static class Page {
+        private List<Media> media;
+    }
+
+    @Getter @Setter
+    public static class Media {
+        private Long id;
+        private  Title title;
+        private CoverImage coverImage;
+        private Double averageScore; // AniList는 100점 만점
+        private StartDate startDate;
+
+        @Getter @Setter
+        public static class Title {
+            private String romaji;
+            private String english;
+            // native는 자바 예약어라서 수정함
+            @com.fasterxml.jackson.annotation.JsonProperty("native")
+            private String nativeTitle;
+        }
+
+        @Getter @Setter
+        public static class CoverImage {
+            private String large;
+        }
+
+        @Getter @Setter
+        public static class StartDate {
+            private Integer year;
+            private Integer month;
+            private Integer day;
+        }
+    }
 }
