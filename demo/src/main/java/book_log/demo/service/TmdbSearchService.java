e02df5cff3aa5db63ccb927c6535efd3c04be04b
Eunseo-ee <2whwhwh@gmail.com>
2026-03-23 13:57:20 +0900

    chore: 사용하지 않는 임포트 정리
    - AnimeSearchService, KakaoSearchService에서 더 이상 사용하지 않는 java.util.Map 임포트 제거

--- demo/src/main/java/book_log/demo/service/TmdbSearchService.java
+++ demo/src/main/java/book_log/demo/service/TmdbSearchService.java
@@ -5,13 +5,11 @@ import book_log.demo.domain.Category;
 import book_log.demo.dto.response.TmdbResponse;
 import book_log.demo.dto.response.UnifiedSearchResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.*;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
-import java.util.Map;
 import java.util.stream.Collectors;
 
 @Service
